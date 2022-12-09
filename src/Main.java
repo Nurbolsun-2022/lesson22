dcffead67384539545f9eb7d61fb185a25274950
Nurbolsun-2022 <tajibaevnurbolsun2022@gmail.com>
2022-12-09 17:03:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        for (int k = 0; k < a; k++) {
+//            if (k == 0) {
+//                System.out.println();
+//            } else
+//                System.out.print(k + " ");
+//            k += 6;
+//        }
+
+
+        int sum = 0;
+        int sum1 = 0;
+        for (int i = 0; i <5 ; i++) {
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    sum+=1;
+                }
+            }
+
+        }
+        sum1+=sum;
+        System.out.println(sum1);
+    }
+}
+
+
